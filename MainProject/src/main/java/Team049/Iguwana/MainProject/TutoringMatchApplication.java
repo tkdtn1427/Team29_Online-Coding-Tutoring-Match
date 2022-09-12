bbf829e3a427bb4797b5e4a4cc6ed7f77115111b
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-12 14:32:10 +0900

    start

---
+++ MainProject/src/main/java/Team049/Iguwana/MainProject/TutoringMatchApplication.java
@@ -0,0 +1,13 @@
+package Team049.Iguwana.MainProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutoringMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutoringMatchApplication.class, args);
+	}
+
+}
