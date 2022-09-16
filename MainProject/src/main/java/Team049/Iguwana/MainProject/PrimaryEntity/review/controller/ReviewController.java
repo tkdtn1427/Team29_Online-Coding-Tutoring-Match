1e245f2fcdc7bc6051b97bab6189c12c124cb58c
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-16 09:39:23 +0900

    Add : review 추가

--- MainProject/src/main/java/Team049/Iguwana/MainProject/PrimaryEntity/review/controller/ReviewController.java
+++ MainProject/src/main/java/Team049/Iguwana/MainProject/PrimaryEntity/review/controller/ReviewController.java
@@ -4,14 +4,20 @@ import Team049.Iguwana.MainProject.PrimaryEntity.review.dto.ReviewDto;
 import Team049.Iguwana.MainProject.PrimaryEntity.review.entity.Review;
 import Team049.Iguwana.MainProject.PrimaryEntity.review.mapper.ReviewMapper;
 import Team049.Iguwana.MainProject.PrimaryEntity.review.service.ReviewService;
+import Team049.Iguwana.MainProject.dto.MultiResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Positive;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("/v1/reivews")
+@RequestMapping("/v1/reviews")
 @Validated
 public class ReviewController {
     public final ReviewService reviewService;
@@ -29,7 +35,28 @@ public class ReviewController {
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-//    @PatchMapping("/update/{review-id}")
-//    public ResponseEntity updateReview(@PathVariable("review-id") long reviewId,
-//                                       @RequestBody )
+    @PatchMapping("/update/{review-id}")
+    public ResponseEntity updateReview(@PathVariable("review-id") long reviewId,
+                                       @RequestBody ReviewDto.Patch patch){
+        patch.setReviewId(reviewId);
+        Review review = reviewMapper.reviewPatchToReview(patch);
+        Review response = reviewService.updateReview(review);
+        return new ResponseEntity(reviewMapper.reviewToReviewResponse(response),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{review-id}")
+    public ResponseEntity deleteReview(@PathVariable("review-id") long reviewId){
+        reviewService.deleteReview(reviewId);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+    // 제대로 가져와지나 테스트 코드
+//    @GetMapping
+//    public ResponseEntity testReview(){
+//        Page<Review> reviewPage = reviewService.findBYTeacherId(0, 2, 1);
+//        List<Review> reviews = reviewPage.getContent();
+//        List<ReviewDto.Response> responseList = reviews.stream().map(review -> {
+//            return reviewMapper.reviewToReviewResponse(review);
+//        }).collect(Collectors.toList());
+//        return new ResponseEntity(new MultiResponseDto<>(responseList,reviewPage),HttpStatus.OK);
+//    }
 }
--- MainProject/src/main/java/Team049/Iguwana/MainProject/PrimaryEntity/review/dto/ReviewDto.java
+++ MainProject/src/main/java/Team049/Iguwana/MainProject/PrimaryEntity/review/dto/ReviewDto.java
@@ -2,6 +2,7 @@ package Team049.Iguwana.MainProject.PrimaryEntity.review.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -20,10 +21,48 @@ public class ReviewDto {
         @NotNull(message = "studentId can't black")
         private long studentId;
 
-        @NotEmpty(message = "studentId can't black")
+        @NotNull(message = "tutoringId can't black")
+        private long tutoringId;
+
+        @NotEmpty(message = "content can't black")
         private String content;
 
-        @NotNull(message = "studentId can't black")
-        private long reputation;
+        @NotNull(message = "reputation can't black")
+        private double reputation;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Patch {
+        @NotNull(message = "reviewId can't black")
+        private long reviewId;
+
+        @NotEmpty(message = "content can't black")
+        private String content;
+
+        @NotNull(message = "reputation can't black")
+        private double reputation;
+
+        private boolean checking;
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Response {
+        private long reviewId;
+
+        private long teacherId;
+
+        private long tutoringId;
+
+        private long studentId;
+
+        private String content;
+
+        private double reputation;
+
+        private LocalDate date;
     }
 }
