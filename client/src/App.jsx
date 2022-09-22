3ef6fc57e4904c716fe922d397d22b197b6bca05
shimeeuisuk <euisuk95@gmail.com>
2022-09-22 09:31:10 +0900

    Add: 로딩페이지 추가

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
+import { Suspense } from 'react';
 import { Global } from '@emotion/react';
 import reset from './styles/reset';
 import './styles/root.css';
-
 import Navbar from './components/nav/Navbar.jsx';
 import Footer from './components/footer/Footer.jsx';
+import Loading from './pages/LoadingPage';
 
 function App() {
   return (
     <div>
       <Global styles={reset} />
-      <Navbar />
-      <Footer />
+      <Suspense fallback={<Loading />}>
+        <Navbar />
+        {/* <Footer /> */}
+      </Suspense>
     </div>
   );
 }
