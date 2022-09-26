95f999423c4ed89380a14a6433de4b5956cbfe97
shimeeuisuk <euisuk95@gmail.com>
2022-09-26 16:22:44 +0900

    Add: 별점후기 컴포넌트 추가

---
+++ client/src/components/star/Stars.jsx
@@ -0,0 +1,23 @@
+// 3점이라면 주황별 3개 회색별 2개가 나오도록 함수를 짜기
+import styled from '@emotion/styled';
+import Star from '../../assets/svg/Star.jsx';
+
+function Stars({ scores, width, height }) {
+  const score = Math.floor(4.3); // 4.3 대신 scores
+  const orgarr = new Array(score).fill(1);
+  const gryarr = new Array(5 - score).fill(1);
+
+  return (
+    <Container>
+      {orgarr.map((_, i) => (
+        <Star key={i} width="50" height="50" color="var(--org)" /> // width, height props로 바꿔야함
+      ))}
+      {gryarr.map((_, i) => (
+        <Star key={i} width="50" height="50" color="var(--liblk)" /> // width, height props로 바꿔야함
+      ))}
+    </Container>
+  );
+}
+
+const Container = styled.div``;
+export default Stars;
