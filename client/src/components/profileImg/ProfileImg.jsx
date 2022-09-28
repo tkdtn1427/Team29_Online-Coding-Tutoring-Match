84f7d4f62ed20987e3c067a84d4bd541936a4015
shimeeuisuk <euisuk95@gmail.com>
2022-09-28 10:07:44 +0900

    Add: profileimg 컴포넌트 추가

---
+++ client/src/components/profileImg/ProfileImg.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+function ProfileImg() {
+  return <Container></Container>;
+}
+
+const Container = styled.div`
+  width: 300px;
+  height: 300px;
+  border-radius: 100%;
+  border: 1px solid var(--blk);
+`;
+
+export default ProfileImg;
