9ddfa1d830ac019c5965f4f3eef626bd4c00e6f2
shimeeuisuk <euisuk95@gmail.com>
2022-09-28 10:06:29 +0900

    Add: infobox 컴포넌트 추가

---
+++ client/src/components/infobox/InfoBox.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import styled from '@emotion/styled';
+import MonthlyCalendar from '../calendar/MonthlyCalendar.jsx';
+import ReviewBox from '../review/ReviewBox.jsx';
+import TabHandler from '../../utils/TabHandler';
+
+function InfoBox() {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const Tab = ['캘린더', '수업 후기'];
+
+  return (
+    <Container>
+      <Tabs>
+        {Tab.map((e, i) => (
+          <div
+            key={i}
+            onClick={() => {
+              TabHandler(i, setCurrentTab);
+            }}
+            className={currentTab === i ? 'selectedtab' : 'unselectedtab'}>
+            {e}
+          </div>
+        ))}
+      </Tabs>
+      {currentTab ? <ReviewBox></ReviewBox> : <MonthlyCalendar></MonthlyCalendar>}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 1200px;
+`;
+
+const Tabs = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  border-bottom: 1px solid var(--blk);
+  padding-bottom: 20px;
+  margin-bottom: 60px;
+  font-size: var(--reg);
+  .selectedtab {
+    color: var(--grn);
+    cursor: pointer;
+  }
+  .unselectedtab {
+    color: var(--blk);
+    cursor: pointer;
+  }
+`;
+export default InfoBox;
