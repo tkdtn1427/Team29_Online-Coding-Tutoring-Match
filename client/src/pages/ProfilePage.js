407b459a86c78406156b7e7dca1a2171d74194bd
shimeeuisuk <euisuk95@gmail.com>
2022-09-28 10:08:47 +0900

    Add: 마이페이지 추가

--- client/src/pages/ProfilePage.js
+++ client/src/pages/ProfilePage.js
@@ -1,7 +1,22 @@
 // 학생용, 선생님용 프로필 컴포넌트 바꾸기
 
+import styled from '@emotion/styled';
+import Profile from '../components/profile/Profile.jsx';
+import MonthlyCalendar from '../components/calendar/MonthlyCalendar.jsx';
+import Schedule from '../components/modal/Schedule.jsx';
+
 function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <Container>
+      <Profile />
+      <MonthlyCalendar />
+      {/* <Schedule /> */}
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  width: 100%;
+`;
 
 export default ProfilePage;
