4b60df156149a88c46058eacc699eb5651ff6ba9
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-09-29 10:24:28 +0900

    set

--- MainProject/src/main/java/Team049/Iguwana/MainProject/PrimaryEntity/review/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package Team049.Iguwana.MainProject.PrimaryEntity.review.repository;
-
-import Team049.Iguwana.MainProject.PrimaryEntity.review.entity.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
