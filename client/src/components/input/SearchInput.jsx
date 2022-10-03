8411f4f970b03a29e53fa157c866e86d10db6518
shimeeuisuk <euisuk95@gmail.com>
2022-10-04 01:31:34 +0900

    Feat: 선생님 닉네임으로 조회 기능 추가

--- client/src/components/input/SearchInput.jsx
+++ client/src/components/input/SearchInput.jsx
@@ -1,11 +1,33 @@
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
+import { useDispatch } from 'react-redux';
 import Search from '../../assets/svg/Search.jsx';
+import { TeachersReducer } from '../../redux/teacherlist/TeachersReducer';
 
-function SearchInput({ width, height, placeholder, ftsize, bdcolor }) {
+function SearchInput({ width, height, placeholder, ftsize, bdcolor, onClick }) {
+  const dispatch = useDispatch();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const changeSearchTerm = e => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    dispatch(TeachersReducer.actions.searchByNickName(searchTerm));
+  }, [searchTerm]);
+
   return (
     <Container width={width} height={height} ftsize={ftsize} bdcolor={bdcolor}>
       <Search width={20} height={20} />
-      <input name="search" type="text" className="fill" placeholder={placeholder} />
+      <input
+        name="search"
+        type="text"
+        className="fill"
+        placeholder={placeholder}
+        onChange={changeSearchTerm}
+        onClick={onClick}
+        value={searchTerm}
+      />
     </Container>
   );
 }
