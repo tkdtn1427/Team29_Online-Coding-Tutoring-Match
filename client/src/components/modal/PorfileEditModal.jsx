2b40b07c6b6517e581825bcaca928d7080778232
shimeeuisuk <euisuk95@gmail.com>
2022-10-03 16:53:17 +0900

    Feat: 프로필 이미지 미리보기, 등록, 수정, 삭제 기능 추가

--- client/src/components/modal/PorfileEditModal.jsx
+++ client/src/components/modal/PorfileEditModal.jsx
@@ -1,10 +1,10 @@
 import Modal from './Modal.jsx';
-import ProfileEditinput from '../form/ProfileEditform.jsx';
+import Profileform from '../form/ProfileEditform.jsx';
 
 function PorfileEditModal({ onClose }) {
   return (
     <Modal onClose={onClose}>
-      <ProfileEditinput onClose={onClose} />
+      <Profileform onClose={onClose} />
     </Modal>
   );
 }
--- client/src/components/profileImg/ProfileImg.jsx
+++ client/src/components/profileImg/ProfileImg.jsx
@@ -1,14 +1,17 @@
 import styled from '@emotion/styled';
 
-function ProfileImg() {
-  return <Container></Container>;
+function ProfileImg({ width, height, src }) {
+  return <Container width={width} height={height} src={src}></Container>;
 }
 
-const Container = styled.div`
-  width: 300px;
-  height: 300px;
+const Container = styled.img`
+  width: ${props => props.width};
+  height: ${props => props.height};
+  src: ${props => props.src};
+  alt: '';
   border-radius: 100%;
   border: 1px solid var(--blk);
+  object-fit: cover;
 `;
 
 export default ProfileImg;
