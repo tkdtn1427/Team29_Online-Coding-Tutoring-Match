c1a9d3bcbb87539e3fb0ea320ff700f4da5720b1
tahupapa <xognl3164@gmail.com>
2022-10-04 10:06:16 +0900

    Test: 데이터 수정

--- client/src/components/refactor/loginForm/LoginForm.jsx
+++ client/src/components/refactor/loginForm/LoginForm.jsx
@@ -4,14 +4,22 @@ import styled from '@emotion/styled';
 import FormController from '../formControl/FormController';
 
 function LoginForm() {
+  const roleOptions = [
+    { key: '회원 종류를 선택하세요', value: '' },
+    { key: '선생님', value: 'teacher' },
+    { key: '학생', value: 'student' },
+  ];
+
   const initialValues = {
     email: '',
     password: '',
+    role: '',
   };
 
   const validationSchema = Yup.object({
     email: Yup.string().email('올바른 이메일을 입력하세요').required('이메일을 입력하세요'),
     password: Yup.string().required('비밀번호를 입력하세요'),
+    role: Yup.string().required('회원 종류를 선택하세요'),
   });
 
   const onSubmit = values => {
@@ -25,6 +33,7 @@ function LoginForm() {
           <Form>
             <FormController control="input" type="email" label="이메일" name="email" />
             <FormController control="input" type="password" label="비밀번호" name="password" />
+            <FormController control="select" label="회원 구분" name="role" options={roleOptions} />
             <button type="submit" disabled={!formik.isValid}>
               login.
             </button>
