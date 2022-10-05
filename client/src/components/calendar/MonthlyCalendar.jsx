4524187a8b285c464086f03524f7e144311d9efc
tahupapa <xognl3164@gmail.com>
2022-10-05 23:51:55 +0900

    Feat: 캘린더 수정 및 레슨 기능 추가

--- client/src/components/calendar/MonthlyCalendar.jsx
+++ client/src/components/calendar/MonthlyCalendar.jsx
@@ -1,35 +1,84 @@
+import { useState, useEffect } from 'react';
 import styled from '@emotion/styled';
-import { useState } from 'react';
+
 import CalendarNav from './CalendarNav.jsx';
 import CalendarBody from './CalendarBody.jsx';
+import Schedule from '../form/Schedule.jsx';
+
+import { SearchLesson } from '../../utils/apis/API/LessonAPI';
+import { TextMode } from '../buttons/ColorMode.jsx';
+import LessonModal from '../modal/LessonModal.jsx';
 
 function MonthlyCalendar() {
-  const [currentDate, setCurrentDate] = useState(new Date());
+  const [isOpen, setIsOpen] = useState(false);
+  const openLessonModal = () => {
+    setIsOpen(!isOpen);
+  };
+
   const [year, setYear] = useState(new Date().getFullYear());
-  const [month, setMonth] = useState(new Date().getMonth());
+  const [month, setMonth] = useState(new Date().getMonth() + 1);
+  const [day, setday] = useState(String(new Date().getDate()));
+
+  // 날짜 변환
+  const changedDate = `${year}-${('00' + month).slice(-2)}-${('00' + day).slice(-2)}`;
+
+  // 오늘 날짜 : YYYY-MM-DD
+  const [currentDate, setCurrentDate] = useState(changedDate);
+
+  // 선택한 날짜 : DD
+  const [selectedDate, setSelectedDate] = useState();
+
+  // 강의 목록 상태
+  const [lesson, setLesson] = useState();
+
+  // 초기상태 오늘로 설정
+  useEffect(() => {
+    SearchLesson({ date: changedDate }).then(res => setLesson(res));
+  }, []);
+
+  // 날짜 클릭시
+  const onDateClick = async day => {
+    const sltdate = `${changedDate.slice(0, 8)}${('0' + day).slice(-2)}`;
+
+    setSelectedDate(day);
+
+    await SearchLesson({ date: sltdate }).then(res => setLesson(res));
+  };
 
   return (
     <Container>
-      <CalendarNav bgColor={'#E4E2BC'} month={month} year={year} setMonth={setMonth} currentDate={currentDate} />
-      <Week>
-        <span>sun</span>
-        <span>mon</span>
-        <span>tue</span>
-        <span>wed</span>
-        <span>thu</span>
-        <span>fri</span>
-        <span>sat</span>
-      </Week>
-      <CalendarBody year={year} month={month} />
+      <CalendarWrp>
+        <CalendarNav bgColor={'#E4E2BC'} month={month} year={year} setMonth={setMonth} currentDate={currentDate} />
+        <Week>
+          <span>sun</span>
+          <span>mon</span>
+          <span>tue</span>
+          <span>wed</span>
+          <span>thu</span>
+          <span>fri</span>
+          <span>sat</span>
+        </Week>
+        <CalendarBody year={year} month={month} day={day} selectedDate={selectedDate} onDateClick={onDateClick} />
+      </CalendarWrp>
+      <Schedule lesson={lesson} />
+      <TextMode mode={'ORANGE'} text={'강의 등록'} onClick={openLessonModal} />
+      {isOpen && <LessonModal onClose={openLessonModal} />}
     </Container>
   );
 }
 
 const Container = styled.div`
   display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 50px;
+`;
+
+const CalendarWrp = styled.div`
+  display: flex;
   flex-direction: column;
   align-items: center;
-  width: 100%;
+  width: fit-content;
   height: auto;
   text-align: center;
 `;
