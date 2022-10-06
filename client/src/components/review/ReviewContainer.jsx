0cbe738614af0c2e6af678aa2c834676828e6e7c
tahupapa <xognl3164@gmail.com>
2022-10-06 18:30:15 +0900

    Feat: 사용자만 리뷰 쓰는 기능 추가

--- client/src/components/review/ReviewContainer.jsx
+++ client/src/components/review/ReviewContainer.jsx
@@ -1,13 +1,31 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import styled from '@emotion/styled';
+
+import { GetUser } from '../../redux/user/UserReducer';
 import ReviewForm from './ReviewForm.jsx';
 import ReviewBox from './ReviewBox.jsx';
 
 function ReviewContainer() {
+  const dispatch = useDispatch();
+  const { user, loading } = useSelector(state => state.user);
+
+  useEffect(() => {
+    dispatch(GetUser());
+  }, []);
+
   return (
-    <Container>
-      <ReviewForm />
-      <ReviewBox />
-    </Container>
+    <>
+      {loading ? (
+        ''
+      ) : (
+        <Container>
+          <ReviewForm user={user} />
+          <ReviewBox user={user} />
+        </Container>
+      )}
+    </>
   );
 }
 
